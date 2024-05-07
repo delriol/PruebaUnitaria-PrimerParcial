40326c3be7a34ef7c150cefa8cf6a4fb65bdb937
delriol <101754321+delriol@users.noreply.github.com>
2024-05-07 20:54:03 -0300

    Add files via upload

---
+++ UnitTest/PruebaUnitaria/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebaUnitaria")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebaUnitaria")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e91d4d11-d273-41b6-a83f-1770590373e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/PruebaUnitaria/PruebaUnitaria.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using primer_parcial_unit_test_procesador_matematico;
+
+
+namespace PruebaUnitaria
+{
+    [TestClass]
+    public class PruebaUnitaria
+    {
+        public class procesadorMatematicoTests
+        {
+            public void DevolverElMayorEntreDosNumeros()
+            {
+                //Arrange
+                var procesador = new ProcesadorMatematico();
+                var primerNumero = 5;
+                var segundoNumero = 10;
+
+                //Act
+                procesador.CalcularMayor(primerNumero, segundoNumero);
+                var resultado = procesador._resultado;
+
+                //Assert - Tiene que devolver el segundo numero
+                Assert.AreEqual(segundoNumero, resultado);
+            }
+            public void Dividendo_ResultadoCorrecto()
+            {
+                //Arrange
+                ProcesadorMatematico procesador = new ProcesadorMatematico();
+                int dividendo = 6;
+                int divisor = 3;
+                int resultadoEsperado = 2;
+
+                //Act
+                procesador.Dividir(dividendo, divisor);
+
+                //Assert - El resultado debe ser un 2
+                Assert.AreEqual(resultadoEsperado, procesador._resultado);
+            }
+        }
+
+    }
+}
